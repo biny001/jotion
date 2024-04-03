2ff6161492eb04b542f41772f955e0323d43fbc3
Binyamin Tamirat <biny01amin@gmail.com>
2024-04-04 00:25:22 +0300

    authntication

--- app/(marketing)/_components/heading.tsx
+++ app/(marketing)/_components/heading.tsx
@@ -1,9 +1,12 @@
 "use client";
 
+import { Spinner } from "@/components/spinner";
 import { Button } from "@/components/ui/button";
 import { ArrowRightIcon } from "@radix-ui/react-icons";
+import { useConvexAuth } from "convex/react";
 
 const Heading = () => {
+  const { isAuthenticated, isLoading } = useConvexAuth();
   return (
     <div className="  max-w-3xl space-y-4">
       <h1 className=" text-3xl sm:text-5xl md:text-6xl font-bold">
@@ -14,10 +17,17 @@ const Heading = () => {
         Jotion is the connected workspace where <br />
         better, faster work happens.
       </h3>
-      <Button className=" ">
-        Enter Jotion
-        <ArrowRightIcon className="h-4 w-4 ml-2" />
-      </Button>
+      {isLoading && (
+        <div className="flex w-full justify-center items-center">
+          <Spinner size="lg" />
+        </div>
+      )}
+      {isAuthenticated && !isLoading && (
+        <Button className=" ">
+          Enter Jotion
+          <ArrowRightIcon className="h-4 w-4 ml-2" />
+        </Button>
+      )}
     </div>
   );
 };
---
+++ convex/auth.config.js
@@ -0,0 +1,8 @@
+export default {
+  providers: [
+    {
+      domain: "https://cosmic-dinosaur-33.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
